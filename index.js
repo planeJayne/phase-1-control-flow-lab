7b57a39108836871baa65efd6d3c1ce6aa1c3d2e
Jasmin <jasmin.parish@outlook.com>
2022-08-08 19:14:25 -0400

    commit tree fa8ff356d89c66724d780cde34c48a5a56184d61

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+const feet = 599;
+function scuberGreetingForFeet(feet) {
+  
+if (feet <= 400){ 
+  return 'This one is on me!'; 
+  }
+
+if (feet >= 400 && feet <= 2000){
+  return 'That will be twenty bucks.';
+  }
+if (feet >=2000 && feet < 2500){
+  return 'I will gladly take your thirty bucks.';
+  }
+  if (feet > 2500){
+  return 'No can do.'
+} 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+
 }
 
 function switchOnCharmFromTip(){
